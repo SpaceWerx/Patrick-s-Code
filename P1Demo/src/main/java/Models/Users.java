8a38f890e724d9c0d8801880c7856a800109018d
user.email <patrick.orwin@revature.com>
2022-05-05 10:20:44 -0400

    added a quick demo of P1 layout

---
+++ P1Demo/src/main/java/Models/Users.java
@@ -0,0 +1,8 @@
+package Models;
+
+public class Users {
+	private int id;
+	private String username;
+	private String password;
+	private Roles role;
+}
---
+++ P1Demo/src/main/java/Service/Reimbursement_Services.java
@@ -0,0 +1,5 @@
+package Service;
+
+public class Reimbursement_Services {
+
+}
